31f3aa167f1b66b2ec805a3d76c95e6e5cb6b228
sixiangqian <sixiangqian@ainirobot.com>
2021-04-22 14:14:14 +0800

    [MDF]fix 冒泡排序

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -5,13 +5,13 @@ import utils.SwapUtils;
 public class BubbleSort implements BaseSort {
     public static void main(String[] args) {
         SortDataCheck sortDataCheck = new SortDataCheck();
-        sortDataCheck.check(new SelectionSort());
+        sortDataCheck.check(new BubbleSort());
     }
 
     @Override
     public void sort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length - 1; j++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     SwapUtils.swap(arr, j, j + 1);
                 }
