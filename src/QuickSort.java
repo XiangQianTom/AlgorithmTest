9c656f08344bc75cb50066130785c33ab3dc57e5
sixiangqian <sixiangqian@orionstar.com>
2022-04-21 20:28:06 +0800

    [MDF]修改快排

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,15 +1,21 @@
 import utils.BaseSort;
 import utils.SortDataCheck;
 
+/**
+ * 快速排序
+ * 任选一个值作为基准元素（通常选取第一个），然后将比它小的排到它的位置之前，
+ * 将比它大的排到它的位置之后，这样就完成了一趟快排（或一次划分）。然后以基准元素左右再次进行快排，最终完成排序。
+ */
 public class QuickSort implements BaseSort {
     public static void main(String[] args) {
-        SortDataCheck.getChecker().check(500, new QuickSort());
+        SortDataCheck.getChecker().check(4, new QuickSort());
     }
 
     @Override
     public void sort(int[] arr) {
 //        sort(arr, 0, arr.length - 1);
-        sort1(arr, 0, arr.length - 1);
+//        sort1(arr, 0, arr.length - 1);
+        sort2(arr, 0, arr.length - 1);
     }
 
     private void sort(int[] arr, int left, int right) {//方法一，左边为轴
@@ -58,5 +64,21 @@ public class QuickSort implements BaseSort {
         arr[j] = temp;
         sort1(arr, left, i - 1);
         sort1(arr, j + 1, right);
+    }
+
+    private void sort2(int[] arr, int left, int right) {//方法三，左边为轴
+        if (left >= right) {
+            return;
+        }
+        int i = left, j = right, temp = arr[left];
+        while (i < j) {
+            while (i < j && arr[j] >= temp) j--;
+            arr[i] = arr[j];
+            while (i < j && arr[i] < temp) i++;
+            arr[j] = arr[i];
+        }
+        arr[i] = temp;
+        sort2(arr, left, i - 1);
+        sort2(arr, i + 1, right);
     }
 }
